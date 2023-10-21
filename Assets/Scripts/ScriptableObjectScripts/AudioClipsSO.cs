2bb84fb703ce3e2a620375b825846191f0bf9502
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-10-22 00:26:25 +0300

    - Updates the AudioClipsSO to be more modular

--- Assets/Scripts/ScriptableObjectScripts/AudioClipsSO.cs
+++ Assets/Scripts/ScriptableObjectScripts/AudioClipsSO.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace ColourMatch
@@ -5,12 +6,16 @@ namespace ColourMatch
     [CreateAssetMenu(fileName = "AudioClips", menuName = "ColourMatch/AudioClips")]
     public class AudioClipsSO : ScriptableObject
     {
-        public AudioClip newGameStartAudioClip;
-        public AudioClip clickAudioCLip;
-        public AudioClip confirmButtonAudioClip;
-        public AudioClip newObstacleSpawnAudioClip;
-        public AudioClip changeColourAudioClip;
-        public AudioClip colourMatchAudioClip;
-        public AudioClip playerImpactAudioClip;
+        public SoundEffectClip[] audioClips;
+
+        public bool HasAudioClip(string audioTag)
+        {
+            return audioClips.Count(x => x.audioTag.Equals(audioTag)) != 0;
+        }
+
+        public AudioClip GetAudioClip(string audioTag)
+        {
+            return audioClips.FirstOrDefault(x => x.audioTag.Equals(audioTag))?.audioClip;
+        }
     }
 }
