792ecca56d9eed28dee55d039090d75a7e238a1e
Graeme Barclay <60799948+BarrusRho@users.noreply.github.com>
2025-06-29 14:45:55 +0300

    - Adds a UIRegistry monobehaviour

---
+++ Assets/_Project/Scripts/UI/Views/UIViewsRegistry.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace ColourMatch
+{
+    public class UIViewsRegistry : MonoBehaviour
+    {
+        public MainMenuView mainMenuView;
+        public DifficultyMenuView difficultyMenuView;
+        public GameHUDView  gameHUDView;
+    }
+}
